0d0b47c0b50d93e5296d3647f83248dc7ef8ad2b
André Porto <andreporto@me.com>
2023-06-21 11:05:40 -0300

    Initial Commit

---
+++ H4KER News/H4KER_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4KER_NewsApp.swift
+//  H4KER News
+//
+//  Created by André Porto on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct H4KER_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
